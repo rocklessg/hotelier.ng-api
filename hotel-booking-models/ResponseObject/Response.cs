b8837f8e9221ccb64de4d1a20decbf1bcf87143c
rocklessg <abdulhafiz.suleiman@decagon.dev>
2021-09-24 04:12:13 +0100

    add Models and Response object completed

---
+++ hotel-booking-models/ResponseObject/Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_booking_models.ResponseObject
+{
+    public class Response<T>
+    {
+        public bool Success { get; set; } = false;
+        public T Data { get; set; }
+        public string ResponseCode { get; set; }
+        public string Message { get; set; }
+    }
+}
