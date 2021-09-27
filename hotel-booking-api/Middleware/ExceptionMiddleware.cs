cfbe8f0e2126c7709f4ad221a4f9253876272f61
obinna asiegbulam <obinna.asiegbulam@decagon.dev>
2021-09-27 08:45:12 +0100

    Add Exception Middleware

--- hotel-booking-api/Middleware/ExceptionMiddleware.cs
+++ hotel-booking-api/Middleware/ExceptionMiddleware.cs
@@ -25,61 +25,14 @@ namespace hotel_booking_api.Middleware
             {
                 await _next.Invoke(context);
             }
-            catch (Exception error)
+            catch (Exception)
             {
-                var response = context.Response;
-                response.ContentType = "application/json";
-                var responseModel = Response<string>.Fail(error.Message);
                 
-                switch (error)
-                {
-                    case BadRequest e:
-                        // custom application error
-                        _logger.LogError(e.StackTrace, e);
-                        response.StatusCode = (int)HttpStatusCode.BadRequest;
-                        break;
-                    case NotFoundException e:
-                        // not found error
-                        _logger.LogError(e.StackTrace, e);
-                        response.StatusCode = (int)HttpStatusCode.NotFound;
-                        responseModel.Message = e.Message;
-                        break;
-                    default:
-                        // unhandled error
-                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                        responseModel.Message = "Internal Server Error. Please Try Again Later.";
-                        break;
-                }
-                var result = JsonConvert.SerializeObject(responseModel);
-                await response.WriteAsync(result);
+                //var result = JsonConvert.SerializeObject(responseModel);
+                //await response.WriteAsync(result);
             }
         }
     }
 
-    public class Response<T>
-    {
-        public T Data {  get; set; }
-        public string Message { get; set; }
-        public string StatusCode {  get; set; }        
-        public bool Succeeded {  get; set; }
-        public static object Fail(string message)
-        {
-            return new Response<T>
-            {
-                Succeeded = false,
-                Message = message
-            };
-        }
-        public override string ToString() => JsonConvert.SerializeObject(this);
-    }
-
-    public class NotFoundException : Exception
-    {
-        
-    }
-
-
-    public class BadRequest : Exception
-    {
-    }
+   
 }
