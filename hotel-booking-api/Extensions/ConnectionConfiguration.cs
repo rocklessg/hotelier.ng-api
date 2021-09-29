01b121d6a1d4a809ef625c48f43aff427c6d98e4
adeosunsam <adeosunsamuel30@gmail.com>
2021-09-29 15:18:56 +0100

    heroku database added for Production

--- hotel-booking-api/Extensions/ConnectionConfiguration.cs
+++ hotel-booking-api/Extensions/ConnectionConfiguration.cs
@@ -21,8 +21,10 @@ namespace hotel_booking_api.Extensions
             var databaseUri = new Uri(connectionUrl);
             string db = databaseUri.LocalPath.TrimStart('/');
             string[] userInfo = databaseUri.UserInfo.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+
             return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};" +
             $"Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
+
         }
 
         public static void AddDbContextAndConfigurations(this IServiceCollection services, IWebHostEnvironment env, IConfiguration config)
