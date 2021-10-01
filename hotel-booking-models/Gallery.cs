9f88fb942308ff2c413fa85fa97b13fc3b9afea6
Abimbola <craftwordltd@gmail.com>
2021-10-01 12:48:11 +0100

    add a new model Gallery

---
+++ hotel-booking-models/Gallery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_booking_models
+{
+    public class Gallery : BaseEntity
+    {
+        public string HotelId { get; set; }
+        public string ImageUrl { get; set; }        
+        public Hotel Hotel { get; set; }
+    }
+}
