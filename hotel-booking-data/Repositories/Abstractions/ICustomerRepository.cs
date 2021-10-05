495cb9e3d5728e096727fb79ff54d254928a21ae
tm-laurenzo <Chibuike.Chidolue@decagon.dev>
2021-10-05 22:08:13 +0100

    nothing serious

--- hotel-booking-data/Repositories/Abstractions/ICustomerRepository.cs
+++ hotel-booking-data/Repositories/Abstractions/ICustomerRepository.cs
@@ -1,13 +1,10 @@
 ﻿using hotel_booking_models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace hotel_booking_data.Repositories.Abstractions
+namespace hotel_booking_data.Repositories.Implementations
 {
-    public interface ICustomerRepository : IGenericRepository<Customer>
+    public interface ICustomerRepository
     {
+        Task<Customer> FindAsync(string customerId);
     }
 }
--- hotel-booking-data/Repositories/Implementations/CustomerRepository.cs
+++ hotel-booking-data/Repositories/Implementations/CustomerRepository.cs
@@ -1,16 +1,31 @@
 ﻿using hotel_booking_data.Contexts;
 using hotel_booking_data.Repositories.Abstractions;
+using hotel_booking_dto.CustomerDtos;
 using hotel_booking_models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace hotel_booking_data.Repositories.Implementations
 {
     public class CustomerRepository : GenericRepository<Customer>, ICustomerRepository
     {
         private readonly HbaDbContext _context;
+        private readonly DbSet<Customer> _customers;
 
         public CustomerRepository(HbaDbContext context) : base(context)
         {
             _context = context;
+            _customers = _context.Set<Customer>();
         }
+
+
+        public async Task<Customer> FindAsync(string customerId)
+        {
+            Customer customer = await _customers.FindAsync(customerId);
+            return customer;
+        }
+
     }
 }
