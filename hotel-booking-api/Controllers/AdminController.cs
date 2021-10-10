6f8758487cbfe25febebd24e6fbd163f6522332c
Ikechukwu <ikechukwu.njoku@decagon.dev>
2021-10-10 14:15:40 +0100

    Modified the response class to use the controllers

--- hotel-booking-api/Controllers/AdminController.cs
+++ hotel-booking-api/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using hotel_booking_core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -24,18 +25,24 @@ namespace hotel_booking_api.Controllers
         }
 
         [HttpGet("get-statistics/manager")]
-        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+       // [Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetManagerStatistics(string managerId)
         {
             _logger.LogInformation($"About Getting Manager Statistics for {managerId}");
 
             var result = await _hotelStatisticsService.GetManagerStatistics(managerId);
             _logger.LogInformation($"Gotten Manager Statistics for {managerId}");
-            return Ok(result);
+            return StatusCode(result.StatusCode, result);
         }
 
         [HttpGet("{managerId}/statistics")]
-        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+       // [Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetHotelManagerStatistics(string managerId)
         {
             _logger.LogInformation($"About Getting Hotel Manager Statistics for {managerId}");
@@ -43,7 +50,7 @@ namespace hotel_booking_api.Controllers
             var result = await _hotelStatisticsService.GetHotelManagerStatistics(managerId);
 
             _logger.LogInformation($"Gotten Hotel Manager Statistics for {managerId}");
-            return Ok(result);
+            return StatusCode(result.StatusCode, result);
         }
     }
 }
