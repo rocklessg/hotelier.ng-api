cbfab663063f43a7a960a5a6c81df9b0a2a0c9ba
obinna asiegbulam <obinna.asiegbulam@decagon.dev>
2021-10-11 11:05:36 +0100

    Update reviews

--- hotel-booking-core/Services/CustomerService.cs
+++ hotel-booking-core/Services/CustomerService.cs
@@ -1,16 +1,61 @@
-﻿using hotel_booking_data.UnitOfWork.Abstraction;
+﻿using System.Threading.Tasks;
+using hotel_booking_data.UnitOfWork.Abstraction;
 using hotel_booking_core.Interfaces;
+using hotel_booking_dto.CustomerDtos;
+using hotel_booking_dto;
+using AutoMapper;
+using System.Net;
+using Microsoft.AspNetCore.Identity;
+using hotel_booking_models;
 
 namespace hotel_booking_core.Services
 {
-    public class CustomerService : ICustomerService
+    public class CustomerService :  ICustomerService
     {
-        IUnitOfWork _unitOfWork;
-        public CustomerService(IUnitOfWork unitOfWork)
+       
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public CustomerService(IUnitOfWork unitOfWork, IMapper mapper,
+            UserManager<AppUser> userManager)
         {
             _unitOfWork = unitOfWork;
-        }       
+            _mapper = mapper;
+            _userManager = userManager;
+        }
 
-        
+        public async Task<Response<string>> UpdateCustomer(string customerId, UpdateCustomerDto updateCustomer)
+        {
+            var response = new Response<string>();
+
+            var customer =  _unitOfWork.Customers.GetCustomer(customerId);
+            if (customer != null)
+            {
+                // Update user details in AspNetAppUser table
+                var user = _userManager.FindByIdAsync(customerId);
+                
+                var result = _mapper.Map(updateCustomer, customer);
+
+
+                _unitOfWork.Customers.Update(result);
+                await _unitOfWork.Save();
+
+                response.Message = "Update Successful";
+                response.StatusCode = (int)HttpStatusCode.OK;
+                response.Succeeded = true;
+                response.Data = customerId;
+                return response;
+
+            }
+
+            response.Message = "Customer Not Found";
+            response.StatusCode = (int)HttpStatusCode.BadRequest;
+            response.Succeeded = false;
+            return response;
+        }
+
+
+
     }
 }
