301aa9fed6602bdd54e9417d70592e63ab5b4977
adeosunsam <adeosunsamuel30@gmail.com>
2021-10-12 10:21:50 +0100

    code clean up

--- hotel-booking-data/Repositories/Implementations/ManagerRequestRepository.cs
+++ hotel-booking-data/Repositories/Implementations/ManagerRequestRepository.cs
@@ -2,11 +2,6 @@
 using hotel_booking_data.Repositories.Abstractions;
 using hotel_booking_models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace hotel_booking_data.Repositories.Implementations
 {
