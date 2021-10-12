7eb1d5b65f150bc965200e83bd7ae2ed84e4b633
adeosunsam <adeosunsamuel30@gmail.com>
2021-10-12 13:45:30 +0100

    conflict resolved

--- hotel-booking-api/Controllers/HotelController.cs
+++ hotel-booking-api/Controllers/HotelController.cs
@@ -70,12 +70,7 @@ namespace hotel_booking_api.Controllers
         [Route("top-deals")]
         public async Task<IActionResult> TopDealsAsync()
         {
-<<<<<<< HEAD
             var response = await _hotelService.GetTopDealsAsync();
-=======
-            var result = await _hotelService.GetTopDealsAsync(paging);
-            var response = new Response<List<RoomInfoDto>>(StatusCodes.Status200OK, true, "List of Top Deals", result);
->>>>>>> 43e12049bd335d0a966befe0ee5dbeb4d593efaa
             return StatusCode(response.StatusCode, response);
         }
 
@@ -83,12 +78,7 @@ namespace hotel_booking_api.Controllers
         [Route("room-by-price")]
         public async Task<IActionResult> GetHotelRoomsByPriceAsync([FromQuery]PriceDto pricing)
         {
-<<<<<<< HEAD
             var response = await _hotelService.GetRoomByPriceAsync(pricing);
-=======
-            var result = await _hotelService.GetRoomByPriceAsync(pricing);
-            var response = new Response<List<RoomInfoDto>>(StatusCodes.Status200OK, true, "List of Rooms By Price", result);
->>>>>>> 43e12049bd335d0a966befe0ee5dbeb4d593efaa
             return StatusCode(response.StatusCode, response);
         }
 
