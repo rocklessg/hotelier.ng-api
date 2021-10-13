d9c96999ef68c3df997d05144721f7509ca246e9
Ayebakuro <ayebakuro.ombu@decagon.dev>
2021-10-13 15:41:15 +0100

    Updated Authorization role

--- hotel-booking-api/Controllers/CustomerController.cs
+++ hotel-booking-api/Controllers/CustomerController.cs
@@ -49,7 +49,7 @@ namespace hotel_booking_api.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [Authorize(Roles = "Customer")]
+        [Authorize(Roles = "Customer, Admin")]
         public async Task<IActionResult> UpdateImage([FromForm] AddImageDto imageDto)
         {
             string userId = HttpContext.User.FindFirst(x => x.Type == ClaimTypes.NameIdentifier).Value;
