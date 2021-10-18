042cfe809c75d2700d220bd66b1c1135059ce441
Reagan Reuben <reagan.reuben@decagon.dev>
2021-10-19 00:56:28 +0100

    Changes Implemented

--- hotel-booking-data/Repositories/Abstractions/IAdminRepository.cs
+++ hotel-booking-data/Repositories/Abstractions/IAdminRepository.cs
@@ -8,10 +8,6 @@ namespace hotel_booking_data.Repositories.Abstractions
     public interface IAdminRepository : IGenericRepository<AppUser>
     {
         IQueryable<Booking> GetAllTransactions(TransactionFilter filter);
-        //IQueryable<Booking> GetTransactionsByQuery(TransactionFilter filter);
-        //IQueryable<Booking> GetTransactionsFilterByDate(TransactionFilter filter);
-        //IQueryable<Booking> GetTransactionsFilterByDate(string year);
-        //IQueryable<Booking> GetTransactionsByHotelAndMonth(TransactionFilter filter);
        Task<Manager> GetManagerById(string id);
     }
 }
